aa07cd4b7450a8ca4c07e0466e8f7c913fc319d3
arysannjayaa <wayanarisanjaya01@gmail.com>
2024-07-21 23:05:05 +0800

    refactor: add some file

--- system/language/english/form_validation_lang.php
+++ system/language/english/form_validation_lang.php
@@ -68,3 +68,4 @@ $lang['form_validation_greater_than']		= 'The {field} field must contain a numbe
 $lang['form_validation_greater_than_equal_to']	= 'The {field} field must contain a number greater than or equal to {param}.';
 $lang['form_validation_error_message_not_set']	= 'Unable to access an error message corresponding to your field name {field}.';
 $lang['form_validation_in_list']		= 'The {field} field must be one of: {param}.';
+$lang['form_validation_edit_unique']= 'The supplied value is already taken.';
